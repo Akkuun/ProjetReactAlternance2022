a3b13b3ff594a3be6524b6d1ea1fdf25ce2369fb
Akkuun <mathisduban@gmail.com>
2022-12-20 14:35:25 +0100

    Stats dynamiques sur toutes les echelles de temps

--- src/component/PageStatistics.js
+++ src/component/PageStatistics.js
@@ -2,6 +2,10 @@ import { getToken } from '../services/Api';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import {Bar, Line} from 'react-chartjs-2';
+import Switch, { SwitchProps } from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import "./stats.scss"
 import 'chart.js/auto'
 import ChartDataLabels from 'chartjs-plugin-datalabels';
@@ -167,24 +171,10 @@ export default function PageStatistics() {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 }
             });
-    
-            let realDataInterval;
-            if (dataInterval == "Day") {
-                realDataInterval = "Hour";
-            }
-    
             let now = moment();
-            let resApi = await axios.get(`https://visionsystem2-apim-dev.azure-api.net/DataProcessing/v1/metricsAggregat/consommation/installation/installSimulated/EC9A8BDDBEEE/${realDataInterval}/Wc/${now.format("YYYY-MM-DD")}/${now.add(1, 'days').format("YYYY-MM-DD")}`, {
-                headers: {
-                    'Authorization': 'Bearer ' + tokenResult.data["access_token"],
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Ocp-Apim-Subscription-Key': ''
-                }
-            });
-    
-            console.log(resApi.data)
-            
+            let realDataInterval;
             let listChildren = [];
+    
             let dataConso = {
                 labels: [],
                 datasets: [
@@ -198,27 +188,178 @@ export default function PageStatistics() {
                 ]
             };
     
-            for(let elem of resApi.data.reverse()) {
-                console.log(elem)
+    
+            if (dataInterval == "Day") {
+                realDataInterval = "Hour";
+    
+                let resApi = await axios.get(`https://visionsystem2-apim-dev.azure-api.net/DataProcessing/v1/metricsAggregat/consommation/installation/installSimulated/EC9A8BDDBEEE/${realDataInterval}/Wc/2022-12-19/2022-12-21`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + tokenResult.data["access_token"],
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Ocp-Apim-Subscription-Key': ''
+                    }
+                });
                 
-                // Axis
-                let timeDataStart = moment(elem.startDateOfMetric)
-                timeDataStart.add(2, 'h')
-                listChildren.push(<div className="tick" key={timeDataStart.format('hh')}>
-                    <span className="day-number">{`${timeDataStart.format('h')}H`}</span>
-                    <span className="day-name">{timeDataStart.format('dddd')}</span>
-                </div>)
+                resApi.data = resApi.data.reverse();
+                let i = 0;
+                if(resApi.data.length>24) i = resApi.data.length-24;
+                
+                for (i; i < resApi.data.length; i++) {
+                    let elem = resApi.data[i];
+                    // Axis
+                    let timeDataStart = moment(elem.startDateOfMetric)
+                    timeDataStart.add(2, 'h')
+                    listChildren.push(<div className="tick" key={timeDataStart}>
+                        <span className="day-number">{`${timeDataStart.format('H')}H`}</span>
+                        <span className="day-name">{timeDataStart.format('ddd')}</span>
+                    </div>)
+                    // Graph
+                    dataConso.labels.push(timeDataStart.format('h'))
+                    dataConso.datasets[0].data.push(elem.sum)
+                }
+            } else if (dataInterval == "Week") {
+                realDataInterval = "Day";
     
-                // Graph
-                dataConso.labels.push(timeDataStart.format('h'))
-                dataConso.datasets[0].data.push(elem.sum)
+                let resApi = await axios.get(`https://visionsystem2-apim-dev.azure-api.net/DataProcessing/v1/metricsAggregat/consommation/installation/installSimulated/EC9A8BDDBEEE/${realDataInterval}/Wc/2022-12-14/2022-12-21`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + tokenResult.data["access_token"],
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Ocp-Apim-Subscription-Key': ''
+                    }
+                });
     
+                resApi.data = resApi.data.reverse();
+                let i = 0;
+                if(resApi.data.length>7) i = resApi.data.length-7;
+    
+                for (i; i < resApi.data.length; i++) {
+                    let elem = resApi.data[i];
+                    console.log(elem)
+                    // Axis
+                    let timeDataStart = moment(elem.startDateOfMetric)
+                    timeDataStart.add(2, 'h')
+                    listChildren.push(<div className="tick" key={timeDataStart}>
+                        <span className="day-number">{`${timeDataStart.format('D')}`}</span>
+                        <span className="day-name">{timeDataStart.format('ddd')}</span>
+                    </div>)
+                    // Graph
+                    dataConso.labels.push(timeDataStart.format('h'))
+                    dataConso.datasets[0].data.push(elem.sum)
+                }
+            } else if (dataInterval == "Year") {
+                realDataInterval = "Year";
+                let resApi = await axios.get(`https://visionsystem2-apim-dev.azure-api.net/DataProcessing/v1/metricsAggregat/consommation/installation/installSimulated/EC9A8BDDBEEE/${realDataInterval}/Wc/2020-10-18/2022-12-21`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + tokenResult.data["access_token"],
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Ocp-Apim-Subscription-Key': ''
+                    }
+                });
+                
+    
+                resApi.data = resApi.data.reverse();
+                let i = 0;
+                if(resApi.data.length>5) i = resApi.data.length-5;
+    
+                for (i; i < resApi.data.length; i++) {
+                    let elem = resApi.data[i];
+                    console.log(elem)
+                    // Axis
+                    let timeDataStart = moment(elem.startDateOfMetric)
+                    timeDataStart.add(2, 'h')
+                    listChildren.push(<div className="tick" key={timeDataStart}>
+                        <span className="day-number">{`${timeDataStart.format('MM')}`}</span>
+                        <span className="day-name">{timeDataStart.format('YYYY')}</span>
+                    </div>)
+                    // Graph
+                    dataConso.labels.push(timeDataStart.format('h'))
+                    dataConso.datasets[0].data.push(elem.sum)
+                }
+            } else if (dataInterval == "Month") {
+                realDataInterval = "Month";
+                let resApi = await axios.get(`https://visionsystem2-apim-dev.azure-api.net/DataProcessing/v1/metricsAggregat/consommation/installation/installSimulated/EC9A8BDDBEEE/${realDataInterval}/Wc/2022-10-18/2022-12-21`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + tokenResult.data["access_token"],
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Ocp-Apim-Subscription-Key': ''
+                    }
+                });
+    
+    
+                resApi.data = resApi.data.reverse();
+                let i = 0;
+                if(resApi.data.length>12) i = resApi.data.length-12;
+    
+                for (i; i < resApi.data.length; i++) {
+                    let elem = resApi.data[i];
+                    console.log(elem)
+                    // Axis
+                    let timeDataStart = moment(elem.startDateOfMetric)
+                    timeDataStart.add(2, 'h')
+                    listChildren.push(<div className="tick" key={timeDataStart}>
+                        <span className="day-number">{`${timeDataStart.format('MM')}`}</span>
+                        <span className="day-name">{timeDataStart.format('YYYY')}</span>
+                    </div>)
+                    // Graph
+                    dataConso.labels.push(timeDataStart.format('h'))
+                    dataConso.datasets[0].data.push(elem.sum)
+                }
             }
-            setAxis(React.createElement('div', {className: 'axis'}, listChildren))
     
-            setGraphDataConso(<Bar options={optionsConso} data={dataConso} />)
+    
+            setAxis(React.createElement('div', {className: 'axis'}, listChildren))
+            setGraphDataConso(<Bar options={optionsConso} data={dataConso}/>)
         }
     }
+    
+    const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+        width: 62,
+        height: 34,
+        padding: 7,
+        '& .MuiSwitch-switchBase': {
+            margin: 1,
+            padding: 0,
+            transform: 'translateX(6px)',
+            '&.Mui-checked': {
+                color: '#fff',
+                transform: 'translateX(22px)',
+                '& .MuiSwitch-thumb:before': {
+                    backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                        '#fff',
+                    )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+                },
+                '& + .MuiSwitch-track': {
+                    opacity: 1,
+                    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+                },
+            },
+        },
+        '& .MuiSwitch-thumb': {
+            backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+            width: 32,
+            height: 32,
+            '&:before': {
+                content: "''",
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                left: 0,
+                top: 0,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                    '#fff',
+                )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+            },
+        },
+        '& .MuiSwitch-track': {
+            opacity: 1,
+            backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+            borderRadius: 20 / 2,
+        },
+    }));
+    
+    
     
     useEffect(() => {
         async function fetchData() {
@@ -234,7 +375,11 @@ export default function PageStatistics() {
                 <br/>
                 <h3>Temperature in °C</h3>
                 <p className="lead">DEVICE_ID</p>
-                
+                <FormGroup className="switch-button">
+                    <FormControlLabel
+                        control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+                    />
+                </FormGroup>
                 <div>
                     <div className="App">
                         <div className="app-header">
@@ -276,10 +421,10 @@ export default function PageStatistics() {
                                     </button>
                                 </li>
                             </ul>
+
                         </div>
                     </div>
                 </div>
-                
                 <div>
                     {graphDataConso}
                 </div>
