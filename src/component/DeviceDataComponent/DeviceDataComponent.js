abc64f836dfb2d3d2a60d934931a4e4dba952bcf
MDU <mathis.duban@wattswater.com>
2023-01-25 16:32:03 +0100

    chose a regarder, totototo apparait seulement aprs avoir fait gettoToken 2fois  de suite, j arrive bien a print les elements de la map mais ça marche pas dans l'affichage car indéfini

--- src/component/DeviceDataComponent/DeviceDataComponent.js
+++ src/component/DeviceDataComponent/DeviceDataComponent.js
@@ -1,29 +1,32 @@
 import * as React from 'react';
 import {useEffect, useState} from 'react';
 
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import Grid from "@mui/material/Grid";
-import List from "@mui/material/List";
+
 import ListItem from "@mui/material/ListItem";
-import DeviceHubIcon from "@mui/icons-material/DeviceHub";
-import CottageIcon from '@mui/icons-material/Cottage';
-import AssessmentIcon from '@mui/icons-material/Assessment';
+
 import {Icon, TextField, Toolbar, Tooltip} from "@mui/material";
-import {AccountCircle} from "@mui/icons-material";
-import moment from "moment";
-import RefreshIcon from '@mui/icons-material/Refresh';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import {AccountCircle, Login} from "@mui/icons-material";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import {DataGrid, GridToolbarContainer, GridToolbarQuickFilter} from "@mui/x-data-grid"
 import Popup from "../popupComponent/popup";
-import {getDataByDeviceID, getListInstallation, getListOfRoomByInstallation, getTokenAPI} from "../../services/Api";
-import Accordion from '@mui/material/Accordion';
+import {
+    getDataByDeviceID,
+    getListInstallation,
+    getListOfRoomByInstallation,
+    getTokenAPI,
+    sendUserConnected
+} from "../../services/Api";
+
 import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import LoadingButton from '@mui/lab/LoadingButton';
 import DeviceDataBubbleComponent from "./DeviceDataBubbleComponent";
+import IconButton from "@mui/material/IconButton";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import Typography from "@mui/material/Typography";
+
+
 //creation item par rapport à une liste de données
 const ListItems = ({items}) =>
     items
@@ -39,31 +42,15 @@ const ListItems = ({items}) =>
 //definition colonne de DataGrid
 
 
-
 const DeviceDataComponent = ({classes}) => {
+
 
     const [a1, setA1] = useState('');
     const [mac, setMac] = useState('');
-    const [accordionOpen, setAccordionOpen] = React.useState(false);
-    const [accordionOpen2, setAccordionOpen2] = React.useState(false);
-    const [mapAccordionOpen, setMapAccordionOpen] = React.useState({});
 
-    const [icons] = useState({
-        installation: [
-            {Icon: CottageIcon},
-        ],
-        device: [
-            {Icon: DeviceHubIcon},
-        ],
-        stats: [
-            {Icon: AssessmentIcon},
-        ]
-    });
-
-
-    useEffect(() => {
-        getToken();
-    }, []);
+    const [install_id, setInstallID] = useState('')
+    const [device_id, setDeviceID] = useState('')
+    const [selectedDevice, setSelectedDevice] = useState(null)
 
 
 //recuperation du a1 pour requêtes
@@ -78,7 +65,6 @@ const DeviceDataComponent = ({classes}) => {
     const MacHandler = async (mac) => {
         if (mac.length === 12) {
             setMac(mac);
-            console.log("entre dna le mac")
             // await getToken(mac);
         } else {
             setMac("");
@@ -96,17 +82,18 @@ const DeviceDataComponent = ({classes}) => {
 
             // Get the list of installation by a A1
             let installationsListResult = await getListInstallation(token, a1)
-            // console.log(installationsListResult.data)
+
 
             let installationsList = [];
+            let tempMapDevicesData = {};
             for (let install of installationsListResult.data) {
 
                 let installationResult = await getListOfRoomByInstallation(token, a1, install)
-                console.log(installationResult.data)
+
                 let devices = [];
                 //for each room, get the data of
                 for (let room of installationResult.data.rooms) {
-                    console.log(room)
+
                     let configurationResult = await getDataByDeviceID(token, room.devices[0].Id_deviceId);
                     newMapConfiguration[room.devices[0].Id_deviceId] = configurationResult.data;
                     let deviceConfigurationData = [];
@@ -121,6 +108,10 @@ const DeviceDataComponent = ({classes}) => {
                             'col3': value.value
                         })
                     }
+
+                    tempMapDevicesData[room.devices[0].Id_deviceId] = deviceConfigurationData;
+                   console.log(room.devices[0].Id_deviceId);
+                    setDeviceID(room.devices[0].Id_deviceId)
                     //devices data
                     devices.push({
                         roomName: room.Rn,
@@ -130,6 +121,7 @@ const DeviceDataComponent = ({classes}) => {
                     })
                 }
 //installation data
+
                 installationsList.push({
                     installation: install,
                     devices: devices
@@ -137,20 +129,50 @@ const DeviceDataComponent = ({classes}) => {
 
             }
             //udate the installationList
-            setInstallationsList(installationsList);
-            console.log("installation")
-            console.log(installationsList)
 
+
+
+
+            console.log("device id");
+            console.log(device_id)
+            setInstallationsList(installationsList);
+            console.log(installationsList)
+            console.log("tempdevicedata avant set")
+            setMapDevicesData(tempMapDevicesData[device_id]);
+            console.log("tempdevicedata après set")
+
+            console.log("tempDeviceData")
+            console.log(tempMapDevicesData)
+            console.log("map deivces data")
+
+
+
+
+            //setMapDevicesData(new Map(mapDevicesData.set(device_id, tempMapDevicesData)))
+            setMapDevicesData(new Array(mapDevicesData[device_id] = tempMapDevicesData))
+            setMapDevicesData(new Array(mapDevicesData[device_id] = tempMapDevicesData))
+          /*  console.log(mapDevicesData[""]["240AC41C7164"][6].col3)
+            console.log(mapDevicesData[""]["240AC41C7164"][18].col3)
+            console.log(mapDevicesData[""]["240AC41C7164"][20].col3)
+            console.log(mapDevicesData[""]["240AC41C7164"][2].col3)
+            console.log(mapDevicesData[""]["240AC41C7164"][1].col3)
+            console.log(mapDevicesData[""]["240AC41C7164"][12].col3)
+            console.log("test")
+            console.log(mapDevicesData);*/
+
+            mapDevicesData.map(elem=> {
+                console.log("tototoot")
+                console.log(elem)
+            })
         } catch (e) {
+
             console.error(e);
         }
     }
 
 
-
-
-
     const [installationsList, setInstallationsList] = useState([]);
+    const [mapDevicesData, setMapDevicesData] = useState([])
 
 
     const addToClipboard = (content) => {
@@ -167,21 +189,66 @@ const DeviceDataComponent = ({classes}) => {
         });
     };
 
-    function manageOpenAccordion(id) {
+    useEffect(() => {
+        //setMapDevicesData(getToken(a1))
+        //console.log(mapDevicesData)
+
+    }, []);
+
+
+    async function DataRefresh() {
+
+        console.log("install ID")
+        console.log(install_id)
+
+        console.log(device_id)
+
+        let dataRefreshed;
+
+        await sendUserConnected(a1, install_id, device_id);
+
+        let token = await getTokenAPI("device");
+        // setTimeout(5000, test());
+
+
+        dataRefreshed = await getDataByDeviceID(token, device_id)
+
+
+        let RowUpdated = [];
+        let added = 0;
         let newMap = {};
-        for (let [key, value] of Object.entries(mapAccordionOpen)) {
-            newMap = {[key]: !value}
+        for (const [key, value] of Object.entries(dataRefreshed.data)) {
+
+            added++;
+            RowUpdated.push({
+                id: added,
+                'col1': value.timestamp,
+                'col2': key,
+                'col3': value.value
+            })
         }
-        setMapAccordionOpen(newMap)
+        console.log("row")
+
+
+        console.log("apres")
+        //setMapDevicesData(mapDevicesData[device_id] = RowUpdated)
+        console.log("toto")
+        //console.log(mapDevicesData[""][device_id][0].col3)
+    }
+
+    function test() {
+        for (let i = 0; i < 10000; i++) {
+            console.log(i)
+        }
     }
 
 
     // creation accodion avec tableaux a partir de la map des donnes obtenu
     return (
 
-        <Grid container spacing={3} marginTop="0%" key={"toto"}
-            >
-            <Grid item xs={9}>
+        <Grid container spacing={3} marginTop="0%" key={Math.random()}
+        >
+            <Grid item xs={9} key={Math.random()}>
                 <form>
                     <div style={{paddingLeft: "35%", justifyContent: "space-between"}}>
                         <AccountCircle sx={{color: 'action.active', mr: 1, my: 2}}/>
@@ -192,95 +259,7 @@ const DeviceDataComponent = ({classes}) => {
                                    onChange={(e) => MacHandler(e.target.value)}/>
                     </div>
                 </form>
-{/*
 
-                {a1.length === 12 && mac.length === 0 ? (<List>
-
-                    <div>
-                        iteration avec map permettant d'afficher tout les boutons ect ..
-                        {installationsList.map(station => (
-                            // permier accordion pour les noms des installations
-                            <Accordion key={station.installation} expanded={mapAccordionOpen[station.installation]}>
-                                <AccordionSummary
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header"
-                                    expandIcon={
-                                        <ExpandMoreIcon
-                                            style={{cursor: 'pointer'}}
-                                            onClick={() => manageOpenAccordion(station.installation)}/>
-                                    }
-                                    sx={{cursor: 'unset !important'}}
-                                >
-                                    <Typography component={'span'}>
-                                        <ListItems items={icons.installation}/>
-                                    </Typography>
-
-                                    <ListItem>
-                                        <Typography onClick={addToClipboard}>
-                                            {station.devices[0].Il}
-                                        </Typography>
-                                    </ListItem>
-
-                                    <Popup classes="popupData"
-                                           value={{"statsInstallation": [a1, station.installation]}}/>
-                                </AccordionSummary>
-
-                                ce que va avoir quand on va cliquer sur l'accordion de l'installation donc les devices
-                                <AccordionDetails>
-                                    {station.devices.map(device => (
-                                        <Accordion key={device.deviceName} expanded={accordionOpen2}>
-                                            <AccordionSummary
-                                                expandIcon={<ExpandMoreIcon/>}
-                                                aria-controls="panel1a-content"
-                                                id="panel1a-header"
-                                                expandIcon={
-                                                    <ExpandMoreIcon
-                                                        style={{cursor: 'pointer'}}
-                                                        onClick={() => setAccordionOpen2(!accordionOpen2)}/>
-                                                }
-                                                sx={{cursor: 'unset !important'}}
-                                            >
-                                                <Typography component={'span'}>
-                                                    <ListItems items={icons.device}/>
-                                                </Typography>
-                                                <ListItem>
-                                                    <ListItemIcon>
-                                                        <Icon/>
-                                                    </ListItemIcon>
-                                                    <span>Room:&nbsp;</span>
-                                                    <Typography onClick={addToClipboard}>
-                                                        {device.roomName}
-                                                    </Typography>
-                                                    <span>&nbsp;Device:&nbsp;</span>
-                                                    <Typography onClick={addToClipboard}>
-                                                        {device.deviceName}
-                                                    </Typography>
-                                                    <ToastContainer/>
-                                                </ListItem>
-
-                                                <Popup classes="popupData"
-                                                       value={{"statsDevice": [a1, station.installation, device.deviceName]}}/>
-
-                                            </AccordionSummary>
-                                            ce qu'on va avoir quand on a cliquer sur le device, le tableau des ty avec
-                                            les données du device en cours
-                                            <AccordionDetails>
-                                                <div style={{height: 300, width: '100%'}}>
-                                                    <DataGrid rows={device.wattsType} columns={columns} components={{
-                                                        Toolbar: CustomToolbar
-                                                    }}/>
-                                                </div>
-                                            </AccordionDetails>
-                                        </Accordion>
-                                    ))}
-                                </AccordionDetails>
-                            </Accordion>
-                        ))}
-                    </div>
-
-
-
-                </List>*/}
 
                 {mac.length === 0 && a1.length === 12 ? <div style={{
                     flexDirection: "row",
@@ -293,30 +272,55 @@ const DeviceDataComponent = ({classes}) => {
                 }}>
 
                     {installationsList.map(station => (
-                       //<DeviceDataBubbleComponent mode={station.devices.wattsType["Cm"]} />
-                       station.devices.map(device=> (
 
-                           <DeviceDataBubbleComponent keyValue={device.wattsType[0].col3}
-                                                      mode={device.wattsType[4].col3}
-                                                      device_name={device.wattsType[20].col3}
-                                                      install_name={device.wattsType[18].col3}
-                                                      temp={((((device.wattsType[1].col3)/10)-32)/1.8).toPrecision(3)}
-                                                      last_updated={device.wattsType[6].col3}
-                                                      data={installationsList}
-                                                      a1={a1}
-                                                      rows={device.wattsType}
-                                                      Installation_Id={station.installation}
-                                                      Device_Id={device.deviceName}
-                           />
+                        station.devices.map(device => (
+
+                            <div style={{
+                                borderStyle: "solid",
+                                borderColor: "red" /* red */,
+                                height: "10%",
+                                width: "45%",
+                                display: "flex",
+                                flexDirection: "row"
+                            }} key={Math.random()}>
 
 
-                       ))
+                                <div><DeviceDataBubbleComponent// keyValue={mapDevicesData[""][device.deviceName][0].col3}
+                                    //                          mode={mapDevicesData[""][device.deviceName][6].col3}
+                                    //                          device_name={mapDevicesData[device.deviceName][20].col3}
+                                    //                          install_name={mapDevicesData[device.deviceName][18].col3}
+                                    //                          temp={((((mapDevicesData[device.deviceName][1].col3) / 10) - 32) / 1.8).toPrecision(3)}
+                                                           // last_updated={mapDevicesData[""]["240AC41C7164"][0].col3}
+                                                             data={installationsList}
+                                                                 a1={a1}
+                                    // rows={mapDevicesData[device.deviceName]}
+                                                               Installation_Id={station.installation}
+                                                                Device_Id={device.deviceName}/>
+
+
+                                </div>
+
+
+                                <IconButton onClick={async () => {
+
+                                    setSelectedDevice(device);
+                                    setDeviceID(device.deviceName);
+                                    setInstallID(station.installation)
+                                    await DataRefresh()
+                                }}> <RefreshIcon sx={{
+                                    position: "left",
+                                    float: "20%",
+                                    Width: "10%",
+                                    height: "10%",
+                                }}/> </IconButton>
+                            </div>
+                        ))
 
                     ))}
 
                 </div> : (
-                    <div>rien</div>)}
-
+                    <div>rien</div>)
+                }
 
 
             </Grid>
