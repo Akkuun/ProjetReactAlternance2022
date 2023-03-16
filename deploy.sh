9b0c3003746cc58ff72750f51b96f8f98fbc2e9b
MDU <mathis.duban@wattswater.com>
2023-03-16 09:01:30 +0100

    ajout script de récupération de data et ajout dans fichier csv, ajout fichier html avec la carte des devices du cloud FENIX

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout master
 
 git add .
 
-git commit -m "release on server V 2.1 ...."
+git commit -m "release on server V 2.2 ...."
 
 echo "pushing on master ....."
 git push origin master
@@ -14,5 +14,12 @@ npm run build
 
 echo "Deploying file to server"
 scp -r build/* dubanm@10.99.3.47:/var/www/10.99.3.47
+echo "Deploying file to server done ! "
+echo "Deploying Script to server"
+scp -r src/component/LocalisationFolder/script.sh dubanm@10.99.3.48:/home/dubanm/LocalisationDevice
+echo "Deploying Script to server done !"
+echo "Deploying Localisation Map Device html file"
+scp -r src/component/LocalisationFolder/Localisation.html dubanm@10.99.3.48:/var/www/html/LocalisationDevice
+echo "Deploying Localisation Map Device html file done !"
 
 echo "Done !"
