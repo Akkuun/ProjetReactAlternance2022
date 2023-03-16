a3f1cf164df086cc382395b1d6590ae3bdab6c80
Akkuun <mathisduban@gmail.com>
2023-03-16 10:10:58 +0100

    rectification script pour rename en index.html

--- src/component/LocalisationFolder/script.sh
+++ src/component/LocalisationFolder/script.sh
@@ -2,5 +2,5 @@ DATA=$(ls -t  ../../brillat/git/backup_cosmos_install/backupFenix/ | head -n 1)
 rm Positions.csv
 node ScriptRecuperationData.js $DATA
 sudo cp Positions.csv /var/www/html/LocalisationDevice
-sudo mv /var/www/html/LocalisationDevice/Localisation.html  /var/www/html/LocalisationDevice/index.html
+
 
