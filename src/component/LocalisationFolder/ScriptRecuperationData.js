a803db8d3df879aa0cbdf6471f91cfc589ef7f06
Akkuun <mathisduban@gmail.com>
2023-03-16 12:18:22 +0100

    ajout device au CSV global DEV PROD DELTA

--- src/component/LocalisationFolder/ScriptRecuperationData.js
+++ src/component/LocalisationFolder/ScriptRecuperationData.js
@@ -1,11 +1,21 @@
 const fs = require("fs");
 
-const lastBackupName = process.argv.slice(2)
+const lastBackupNameFenix = process.argv.slice(2)
+const lastBackupNameDeltacalor = process.argv.slice(3)
+lastBackupNameDeltacalor.pop()
+lastBackupNameDeltacalor.pop()
 
-function getAllData() {
-    let directory = `/home/brillat/git/backup_cosmos_install/backupDeltacalor/${lastBackupName}`;
-    let count=0;
-    fs.writeFileSync(`/home/dubanm/LocalisationDevice/Positions.csv`, "id,latitude,longitude\r\n")
+const lastBackupNameDev = process.argv.slice(4)
+lastBackupNameDev.pop()
+const lastBackupNameProd = process.argv.slice(5)
+//vide donc erreur
+//const lastBackupNameGkp = process.argv.slice(6)
+
+
+const listeCloud=["Deltacalor","Dev","Prod"]
+let count=0;
+function ExtractDataFromCloudBackupToCSV(directory) {
+    console.log(directory)
     for (const file of fs.readdirSync(`${directory}/`)) {
         let data = fs.readFileSync(`${directory}/${file}`, "utf-8");
         //On fait d'abord JSON Parse pour récupérer le contenu du JSON
@@ -13,10 +23,8 @@ function getAllData() {
         // qui renvoie un tableau d'élement après avoir séparé la string de la ,
         let Coordinate = JSON.stringify(JSON.parse(data).addressGPS)
 
-        console.log(count++)
-        var CoordinateArray = Coordinate.split(",")
-        console.log(data)
-
+        console.log(listeCloud[count] + Coordinate)
+        if (Coordinate!=null || Coordinate!==undefined) var CoordinateArray = Coordinate.split(",")
 
 ///filtre pour enlever les valeurs parasites
         if (!(CoordinateArray.includes("null")) && !(CoordinateArray.includes(null)) && !(CoordinateArray.includes("")) &&
@@ -26,11 +34,30 @@ function getAllData() {
             let lag = CoordinateArray[1].replace('"', '')
 
             //ajoute au fichier csv les coordonnées
-            fs.appendFileSync(`Positions.csv`, `${JSON.parse(data).id},${long},${lag}\r\n`)
+            fs.appendFileSync(`Positions.csv`, `${JSON.parse(data).id},${long},${lag},${listeCloud[count]}\r\n`)
 
         }
 
     }
+    count++;
+}
+
+function getAllData() {
+
+    fs.writeFileSync(`/home/dubanm/LocalisationDevice/Positions.csv`, "id,latitude,longitude,cloud\r\n")
+
+    let directoryCloud = [`/home/brillat/git/backup_cosmos_install/backupDeltacalor/${lastBackupNameDeltacalor}`,
+        `/home/brillat/git/backup_cosmos_install/backupWattsDev/${lastBackupNameDev}`,
+        `/home/brillat/git/backup_cosmos_install/backupWattsProd/${lastBackupNameProd}`
+        // `/home/brillat/git/backup_cosmos_install/Gkp/${lastBackupNameGkp}`
+        ]
+    console.log("directoryCloud" + directoryCloud)
+
+   directoryCloud.forEach( element =>{
+       console.log("element" + element)
+      ExtractDataFromCloudBackupToCSV(element)
+
+   })
 
 
 }
--- src/component/LocalisationFolder/script.sh
+++ src/component/LocalisationFolder/script.sh
@@ -1,6 +1,11 @@
-DATA=$(ls -t  ../../brillat/git/backup_cosmos_install/backupFenix/ | head -n 1)
+LAST_DATE_FENIX=$(ls -t  ../../brillat/git/backup_cosmos_install/backupFenix/ | head -n 1)
+LAST_DATE_DELTACALOR=$(ls -t  ../../brillat/git/backup_cosmos_install/backupDeltacalor/ | head -n 1)
+LAST_DATE_DEV=$(ls -t  ../../brillat/git/backup_cosmos_install/backupWattsDev/ | head -n 1)
+LAST_DATE_PROD=$(ls -t  ../../brillat/git/backup_cosmos_install/backupWattsProd/ | head -n 1)
+#LAST_DATE_GKP=$(ls -t  ../../brillat/git/backup_cosmos_install/backupGkp/ | head -n 1)
+
 rm Positions.csv
-node ScriptRecuperationData.js $DATA
+node ScriptRecuperationData.js $LAST_DATE_FENIX $LAST_DATE_DELTACALOR $LAST_DATE_DEV $LAST_DATE_PROD #$LAST_DATE_GKP
 sudo cp Positions.csv /var/www/html/LocalisationDevice
 
 
