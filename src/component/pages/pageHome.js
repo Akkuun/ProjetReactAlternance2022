d4f0e93c16b0473e057b8f8e5070fe5a2b436da8
Akkuun <mathisduban@gmail.com>
2023-03-21 12:30:39 +0100

    release on server V 2.4 ....

--- src/component/pages/pageHome.js
+++ src/component/pages/pageHome.js
@@ -1,13 +1,29 @@
 import React from "react"
-
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const pageHome = ()=> {
     return (
 
-        <div>
 
-            home
-        </div>
+
+<div style={{width: "78%"}}>
+        <Player
+          loop autoplay  src='https://assets7.lottiefiles.com/packages/lf20_0uwehwnd.json'
+            className="player"
+        />
+    <div style={{fontFamily: "Georgia, serif",
+        fontSize: "40px",
+        letterSpacing: "0px",
+        wordSpacing:" 0.2px",
+        color:" #000000",
+        fontWeight:" 700",
+       textDecoration:" none",
+        fontStyle: "normal",
+        fontVariant: "normal",
+        textTransform: "none"}}>
+        EAGLE 3000
+    </div>
+</div>
 
     )
 
