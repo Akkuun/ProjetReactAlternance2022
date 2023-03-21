4d1ede33a093b26e1197215eca5772dca7798cc3
MDU <mathis.duban@wattswater.com>
2023-03-21 12:30:39 +0100

    release on server V 2.4 ....

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout master
 
 git add .
 
-git commit -m "release on server V 2.3 ...."
+git commit -m "release on server V 2.4 ...."
 
 echo "pushing on master ....."
 git push origin master
