bead6c2dfc78a56b23ad7161f7d5464461b34d1e
MDU <mathis.duban@wattswater.com>
2023-06-13 13:08:33 +0200

    ajout de courbe issue d'une reqÃuête influxDB

--- src/component/InfluxDb/InfluxDBComponent.js
+++ src/component/InfluxDb/InfluxDBComponent.js
@@ -1,45 +1,22 @@
-import React from "react";
-import {InfluxDB} from "@influxdata/influxdb-client";
-import {FluxTableMetaData} from "@influxdata/influxdb-client";
+import React, {useState} from "react";
 
-const InfluxDBComponent = ()=> {
-    const {InfluxDB} = require('@influxdata/influxdb-client')
+import LineChart from "./LineChart";
 
-    const token = 'SN12c-NOIptEN7rTWtFcnCLPkw2yxTNyDV6OFf4t-g0wmHg3AJhzrH0M9AXFX0qKpFwv9GpRhTVe2uTzhsr1eg=='
-    let org = `Watts`
-    const url = 'http://10.99.3.47:8086'
-    let bucket = `StatsWattsType`
 
-    const client = new InfluxDB({url, token})
-    let queryClient = client.getQueryApi(org)
+const InfluxDBComponent = () => {
 
-    const fluxQuery = `from(bucket: "StatsWattsType")
-          |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-          |> filter(fn: (r) => r["cloud"] == "Dev")
-          |> filter(fn: (r) => r["wattsType"] == "Cm")
-          |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)
-          |> yield(name: "mean")`
 
-    queryClient.queryRows(fluxQuery, {
-        next: (row, tableMeta) => {
-            const tableObject = tableMeta.toObject(row)
-            console.log(tableObject)
-        },
-        error: (error) => {
-            console.error('\nError', error)
-        },
-        complete: () => {
-            console.log('\nSuccess')
-        },
-    })
 
     return (
+        <div style={{width:"50%",height:"50%", paddingLeft:"25%"}}>  <LineChart /> </div>
+
+    )
 
 
-    <div> salut</div>
-
-)
 }
 
 
 export default InfluxDBComponent
+
+
+
