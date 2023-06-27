02eb543f9be64b04551fb2d7e950e29e506f3b4f
Akkuun <mathisduban@gmail.com>
2023-06-27 11:14:30 +0200

    release on server V 3.1 ....

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout master
 
 git add .
 
-git commit -m "release on server V 2.5 ...."
+git commit -m "release on server V 3.1 ...."
 
 echo "pushing on master ....."
 git push origin master
